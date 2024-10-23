bfb227d179f9379ce6fcd5ccd3f53c8f51ae31ee
Sean Murphy <sean@iamseanmurphy.com>
2024-10-23 12:51:41 +0000

    5.1.8

--- wp-scheduled-posts/includes/Admin/Settings/assets/js/admin.asset.php
+++ wp-scheduled-posts/includes/Admin/Settings/assets/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'b9e0e0fb7ab7f70719d1');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '5f6bb016b8f906c67265');
